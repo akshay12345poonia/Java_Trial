e8a003e1726c37a22da1ad21826522de52592985
unknown <akshay12345poonia@gmail.com>
2024-04-03 11:53:50 +0530

    first commit

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+package github;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 12;
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				System.out.print("*");
+				}
+			System.out.println();
+		}
+
+	}
+
+}
